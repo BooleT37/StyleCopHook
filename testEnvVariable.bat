e5c9db1f2e795b39f272ba2f3640998ffed5e1f0
Alexey Levin <lad@skbkontur.ru>
2017-05-12 00:09:28 +0500

    remove StyleCop path environmental variable from testEnvVariable.bat

--- testEnvVariable.bat
+++ testEnvVariable.bat
@@ -4,9 +4,4 @@ if "%HG_HOOKS_PATH%"=="" (
 ) else (
     echo HG_HOOKS_PATH is %HG_HOOKS_PATH%
 )
-if "%HG_STYLECOP_PATH%"=="" (
-    echo HG_STYLECOP_PATH isn't set
-) else (
-    echo HG_STYLECOP_PATH is %HG_STYLECOP_PATH%
-)
 pause
